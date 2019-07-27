be1f9a245368b81795bbfc67091d1a4ff36cb471
salah eddin alshaal <salshaal@gmail.com>
2019-07-27 22:04:47 +0200

    added dark theme for story book

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,6 +1,13 @@
 import { configure } from '@storybook/react';
+import { addParameters } from '@storybook/react';
+import { themes } from '@storybook/theming';
 
 function loadStories() {
+  // addParameters({
+  //   options: {
+  //     theme: themes.dark,
+  //   },
+  // });
   require('../src/stories');
 }
 
